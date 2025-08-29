0e306e8851d17432a67bc41ae783c953e03e28d4
jambyyyy <jambyaranton.gaspayad@my.smciligan.edu.ph>
2025-08-29 22:24:44 +0800

    Update me.php

--- me.php
+++ me.php
@@ -1,15 +1,19 @@
 <?php
 require __DIR__ . '/config.php';
+json_header();
 
-if (!empty($_SESSION['customer_id'])) {
-  echo json_encode([
-    'ok' => true,
-    'customer' => [
-      'id' => (int)$_SESSION['customer_id'],
-      'full_name' => $_SESSION['customer_name'] ?? '',
-      'email' => $_SESSION['customer_email'] ?? ''
-    ]
-  ]);
-} else {
-  echo json_encode(['ok' => false]);
+if (!isset($_SESSION['customer_id'])) {
+  echo json_encode(['ok' => false, 'error' => 'Not logged in']);
+  exit;
 }
+
+echo json_encode([
+  'ok' => true,
+  'customer' => [
+    'customer_id' => $_SESSION['customer_id'],
+    'full_name' => $_SESSION['customer_name'] ?? '',
+    'email' => $_SESSION['customer_email'] ?? '',
+    'username' => $_SESSION['customer_username'] ?? ''
+  ]
+]);
+?>
